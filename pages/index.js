cd81666a6a9ac7dc31a62eab7c0d5638f7bb2934
“mluena” <maluenarod@gmail.com>
2020-10-15 18:22:28 +0200

    WIP: next and basic set up

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const HomePage = () => (
+  <section className="p-home">
+    <p>Home</p>
+  </section>
+);
+
+export default HomePage;
---
+++ pages/map/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const MapPage = () => (
+  <section className="p-map">
+    <p>Map</p>
+  </section>
+);
+
+export default MapPage;
