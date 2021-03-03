1b4ea926d845068cb81cd092e841391eaa79ca4c
mluena <maluenarod@gmail.com>
2021-03-03 17:11:03 +0100

    transifex snippet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import wrapper from 'config/store';
 
@@ -7,7 +8,22 @@ import wrapper from 'config/store';
 import 'styles/index.css';
 
 const GreenEnergyDataApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Head>
+      {typeof window !== 'undefined' && (
+      <script>
+        {window.liveSettings = {
+          detectlang: true,
+          dynamic: true,
+          picker: '#transifex-picker',
+          api_key: process.env.NEXT_PUBLIC_TRANSIFEX_API_TOKEN,
+        }}
+      </script>
+      )}
+      {typeof window !== 'undefined' && (<script type="text/javascript" src="//cdn.transifex.com/live.js"></script>)}
+    </Head>
+    <Component {...pageProps} />
+  </>
 );
 
 export default wrapper.withRedux(GreenEnergyDataApp);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React, {
-  FC,
+  FC
 } from 'react';
 
 // components
@@ -9,11 +9,14 @@ import Head from 'components/head';
 type HomePageProps = {
 };
 
-const HomePage: FC<HomePageProps> = () => (
+const HomePage: FC<HomePageProps> = () => {
+  return (
   <StaticPage className="static-custom">
     <Head title="Welcome to Green Energy Data Platform" />
     <p className="text-green-600">Home</p>
+    <div id="transifex-picker" />
   </StaticPage>
 );
+  }
 
 export default HomePage;
