6f00570d1462c9add864e03b4cd02b54d775490d
mluena <maluenarod@gmail.com>
2021-03-05 09:19:44 +0100

    window declared as glogal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,18 +7,31 @@ import wrapper from 'config/store';
 // styles
 import 'styles/index.css';
 
+declare global {
+  interface Window {
+    liveSettings: {
+      detectlang: boolean,
+      dynamic: boolean,
+      picker: string,
+      api_key: string,
+    }
+  }
+};
+
+typeof window !== 'undefined' && (window.liveSettings = window.liveSettings);
+
 const GreenEnergyDataApp = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       {typeof window !== 'undefined' && (
-      <script>
-        {window.liveSettings = {
-          detectlang: true,
-          dynamic: true,
-          picker: '#transifex-picker',
-          api_key: process.env.NEXT_PUBLIC_TRANSIFEX_API_TOKEN,
-        }}
-      </script>
+        <script>
+          {window.liveSettings = {
+            detectlang: true,
+            dynamic: true,
+            picker: '#transifex-picker',
+            api_key: process.env.NEXT_PUBLIC_TRANSIFEX_API_TOKEN,
+          }}
+        </script>
       )}
       {typeof window !== 'undefined' && (<script type="text/javascript" src="//cdn.transifex.com/live.js"></script>)}
     </Head>
