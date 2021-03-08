32ea36b72bb2e8aa2b2568a35ad94c1104a051b3
mluena <maluenarod@gmail.com>
2021-03-08 18:40:14 +0100

    WIP: styles signin

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -15,7 +15,7 @@ import {
 import StaticPage from 'layout/static-page';
 import Head from 'components/head';
 
-export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> { }
 
 type LoginProps = {
   onChange: (type: string, e: string) => void;
@@ -38,40 +38,58 @@ const SigninPage: FC<LoginProps> = ({
   };
 
   return (
-    <StaticPage className="static-custom">
+    <StaticPage className="h-screen static-custom p-10 bg-sign-up">
       <Head title="Welcome to Green Energy Data Platform" />
       <div className="inline-flex">
-        <section>
+        <section className="text-white">
           <h1>Sign in</h1>
           <p>Create an account to explore more about GEDP data insights</p>
+          <div className="w-full h-0.2 bg-gradient-to-r from-white to-white-50" />
           <p>Don&apos;t have an account?</p>
-          <Link href={{ pathname: '/sign-up' }}>Sign up</Link>
+          <Link href={{ pathname: '/sign-up' }}>
+            <a href="/sign-up" className="border rounded-lg border-white">Sign up</a>
+          </Link>
         </section>
-        <section className="inline-flex flex-col">
-          <form method="post">
-            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-            <label htmlFor="email">Your email is:</label>
+        <section className="bg-white rounded px-5 py-8">
+          <form method="post" className="inline-flex flex-col p4">
             <input
-              id="email"
-              name="email"
-              type="email"
-              value={credentials.email}
-              onChange={(e) => handleChange('email', e)}
-              required
+              name="csrfToken"
+              type="hidden"
+              defaultValue={csrfToken}
+              className="placeholder-gray-300 placeholder-opacity-50 border-gradient-to-r from-white to-white-50 focus:border-b-2"
             />
-            <label htmlFor="password">Enter your password:</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              value={credentials.password}
-              onChange={(e) => handleChange('password', e)}
-              required
-            />
+            <label htmlFor="email">
+              Your email is:
+              <input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Write your email account"
+                className="border-b-2"
+                value={credentials.email}
+                onChange={(e) => handleChange('email', e)}
+                required
+              />
+            </label>
+            <label htmlFor="password">
+              Enter your password:
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Write the associated password"
+                className="w-full placeholder-transparent border-b-2"
+                value={credentials.password}
+                onChange={(e) => handleChange('password', e)}
+                required
+              />
+            </label>
+            <a href="" className="underline">I don't remember my password</a>
             <div>
               <button
                 type="submit"
                 aria-label="Sign in"
+                className="text-white border rounded-lg bg-black"
                 onClick={(evt) => {
                   evt.preventDefault();
                   signIn('credentials');
