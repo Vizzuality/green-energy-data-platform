b4b5195707cebbc1cf20363be7d368218f7f8f5e
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-03-10 09:36:04 +0100

    removes unnecessary catch

--- services/widgets.tsx
+++ services/widgets.tsx
@@ -1,9 +1,6 @@
 import API from 'utils/api';
 
 const fetchWidgets = () => API.get('/widgets')
-  .then(({ data }) => data)
-  .catch((e) => {
-    console.log(e, 'error');
-  });
+  .then(({ data }) => data);
 
 export default fetchWidgets;
