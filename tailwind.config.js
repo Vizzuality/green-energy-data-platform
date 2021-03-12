30b4912a5c066efa81c61ed72621b4f18a2b30a3
mluena <maluenarod@gmail.com>
2021-03-12 08:40:38 +0100

    fonts, ui kit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,60 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   darkMode: false,
   theme: {
-    colors: {
-      ...colors,
-      color1: '#026BFE',
-      color2: '#FD1B30',
-      color3: '#6907E8',
-    },
     pseudo: {
       before: 'before',
       after: 'after',
       'not-first': 'not(:first-child)',
     },
+    extend: {
+      colors: {
+        color1: '#6905E8',
+        color2: '#006BFE',
+        color3: '#FD1A2F',
+        gray1: '#EAEFF3',
+        gray2: '#3A3F59',
+        gray3: '#2F3031',
+        gray4: '#C4C4C4',
+        // chart colors
+        color4: '#A880FF',
+        color5: '#29C2DA',
+        color6: '#FEB961',
+        color7: '#00A3FE',
+        color8: '#FFACE3',
+        color9: '#1D8190',
+        color10: '#FF7629',
+        color11: '#CC1771',
+        color12: '#8E9195',
+        color13: '#FF61CA',
+        color14: '#7898E1',
+        color15: '#C5C6C8',
+        color16: '#B9EBD7',
+        color17: '#75BBF5',
+        color18: '#E580B5',
+        color19: '#FFD675',
+        color20: '#FFAC8C',
+        color21: '#0C3CC4',
+        color22: '#041F5A',
+        color23: '#5AB015',
+        color24: '#A97500',
+      },
+      spacing: {
+        3.75: '0.938rem',
+        6.5: '1.563rem',
+      },
+      width: {
+        7.5: '1.875rem',
+      },
+      height: {
+        7.5: '1.875rem',
+      },
+      borderRadius: {
+        'rounded-2lg': '0.625rem',
+      },
+    },
   },
   variants: {
-    extend: {},
+
   },
 };
