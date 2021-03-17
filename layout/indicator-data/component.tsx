ec5535a0d2959af707ccdc3528635653d8a5f83a
mluena <maluenarod@gmail.com>
2021-03-17 08:09:05 +0100

    layout widget display

--- layout/indicator-data/component.tsx
+++ layout/indicator-data/component.tsx
@@ -1,6 +1,7 @@
 import React, {
   FC,
 } from 'react';
+import cx from 'classnames';
 
 // components
 import VisualizationsNav from 'components/visualizations-nav';
@@ -8,27 +9,34 @@ import Dropdown from 'components/select/component';
 import Filters from 'components/filters';
 import DataSource from 'components/data-source';
 
+import { indicatorsList, datesList } from '../../constants';
+
 interface IndicatorDataProps {
   color: string;
+  className?: string;
 }
 
 const IndicatorData: FC<IndicatorDataProps> = ({
   color,
+  className,
 }: IndicatorDataProps) => (
-  <div className="bg-white rounded-2.5xl text-black divide-y divide-gray mx-13 shadow-sm -mt-40">
+  <div className={cx('bg-white rounded-2.5xl text-black divide-y divide-gray shadow-sm',
+    { [className]: className })}
+  >
     <VisualizationsNav className="px-32 w-full" selected={'Line'} color={color} />
     <div className="flex px-32 py-11 w-full">
       <section className="flex-1 mr-8">
         <div className="flex items-center w-full justify-between">
-          <h2 className="text-3.5xl max-w-sm">Overall energy balance</h2>
-          <Dropdown label="Change indicator:" />
+          <h2 className="text-3.5xl max-w-sm font-bold">Overall energy balance</h2>
+          <Dropdown items={indicatorsList} label="Change indicator" icon="triangle_border" />
         </div>
-        Change indicator:
-        <Dropdown />
+        <Dropdown items={datesList} icon="calendar" />
         <p>Widget</p>
       </section>
       <section className="flex flex-col justify-between">
-        <Filters />
+        <div>
+          <Filters className="mb-4" color={color} />
+        </div>
         <DataSource />
       </section>
     </div>
