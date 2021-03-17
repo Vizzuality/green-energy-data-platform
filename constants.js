cab64247f719d2d1c2b42c903b8264d4f0d23804
mluena <maluenarod@gmail.com>
2021-03-17 08:05:03 +0100

    constants

---
+++ constants.js
@@ -0,0 +1,27 @@
+export const groups = [
+  {
+    id: 'energy',
+    name: 'Energy',
+    status: 'active',
+    color: 'color1',
+  },
+  {
+    id: 'socio-economic',
+    name: 'Socio-economic',
+    status: 'disabled',
+    color: 'color2',
+  },
+  {
+    id: 'coal-power-plant',
+    name: 'Coal power plant',
+    status: 'disabled',
+    color: 'color3',
+  },
+];
+
+export const relatedIndicators = ['widget', 'widget', 'widget', 'widget', 'widget'];
+
+export const indicatorsList = ['indicator1', 'indicator2', 'indicator3', 'indicator4'];
+export const datesList = ['1990', '2000', '2010', '2020'];
+
+export default { groups, relatedIndicators, indicatorsList };
