e9d47af0c7056898d18b19df72ed84a34b74f5a8
mluena <maluenarod@gmail.com>
2021-03-17 08:32:03 +0100

    color changed in Download component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,12 @@ import React, {
 // components
 import StaticPage from 'layout';
 import Head from 'components/head';
-<<<<<<< HEAD
 import Footer from 'components/footer';
-=======
 
-<<<<<<< HEAD
-const LanguageSelect = dynamic(() => import('../components/language-select'), { ssr: false });
->>>>>>> c9989db... adjust buttons to styles out of ui-kit
-
-=======
->>>>>>> 66e05f7... remove unneccesary import
 const HomePage: FC = () => (
   <StaticPage className="static-custom">
     <Head title="Welcome to Green Energy Data Platform" />
-<<<<<<< HEAD
     <Footer />
-=======
-    <p className="text-5xl bold">Green energy data platform</p>
-    <LanguageSelect />
->>>>>>> c9989db... adjust buttons to styles out of ui-kit
   </StaticPage>
 );
 
