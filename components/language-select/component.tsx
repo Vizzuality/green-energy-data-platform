4d9a5b5c22262a1dd442e7e40a9f7b623f9962a8
mluena <maluenarod@gmail.com>
2021-03-18 09:14:06 +0100

    language select updated

--- components/language-select/component.tsx
+++ components/language-select/component.tsx
@@ -5,8 +5,6 @@ import cx from 'classnames';
 // components
 import Icon from 'components/icon';
 
-import Icon from 'components/icon';
-
 const LanguageSelect = () => {
   const [languages, setLanguages] = useState([]);
   const items = languages;
