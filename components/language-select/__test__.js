0aec3ccb56081c29e5559804af7e8941280b306e
mluena <maluenarod@gmail.com>
2021-03-18 08:49:07 +0100

    language selector test

--- components/language-select/__test__.js
+++ components/language-select/__test__.js
@@ -1,5 +1,5 @@
-describe('Translations toggle successfylly', () => {
-  it('Translate from EN to ZH CH', () => {
+describe('Translations toggle successfully', () => {
+  it('Translates from EN to ZH CH', () => {
     cy.intercept({
       url: '/',
     });
@@ -10,7 +10,9 @@ describe('Translations toggle successfylly', () => {
         expect(text).to.equal('Select language');
         cy.get('[data-code="zh_CN"]').click();
       });
+  });
 
+  it('Translates from ZH CH yo EN', () => {
     cy.get('#downshift-0-toggle-button').click()
       .invoke('text')
       .then((text) => {
