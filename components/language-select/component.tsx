8f534e343a8fb5838961c90822fd1488f732cce6
mluena <maluenarod@gmail.com>
2021-04-19 10:45:29 +0200

    ricon removed

--- components/language-select/component.tsx
+++ components/language-select/component.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useSelect } from 'downshift';
-import cx from 'classnames';
 
 // components
 import Icon from 'components/icon';
@@ -51,12 +50,7 @@ const LanguageSelect = () => {
       >
         <Icon className="text-white" ariaLabel="world ball" name="language" size="lg" />
         <span className="px-3">Select language</span>
-        <Icon
-          className={cx('fill-current text-white', { 'transform rotate-180': !isOpen })}
-          ariaLabel="arrow"
-          name="filled_triangle"
-          size="sm"
-        />
+
       </button>
       <ul
         className="flex-col bg-gray1 absolute bottom-7 w-full pl-8"
