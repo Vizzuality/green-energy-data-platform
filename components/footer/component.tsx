d87e88fa822b49b36f9a583d91653f081bf73f77
mluena <maluenarod@gmail.com>
2021-05-06 08:55:43 +0200

    header removed

--- components/footer/component.tsx
+++ components/footer/component.tsx
@@ -16,7 +16,7 @@ const Footer: FC<FooterProps> = ({
 
   >
     <div className="flex w-full justify-around p-2 items-center">
-      <img alt="GEDP" src="images/logo_GEDP.svg" className="w-28 h-auto" />
+      <img alt="GEDP" src="/images/logo_GEDP.svg" className="w-28 h-auto" />
       <div className="flex divide-x">
         <p className="px-4">Privacy Policy</p>
         <p className="px-4">Terms of Service</p>
