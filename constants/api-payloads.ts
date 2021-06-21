2591d99a2aab0d26f10883c1f4980e491815895d
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-06-21 17:33:13 +0200

    nextjs API workaround

---
+++ constants/api-payloads.ts
@@ -0,0 +1,229 @@
+export const GROUPS = [
+  {
+    id: 1,
+    slug: 'energy',
+    title: 'Energy',
+    subtitle: 'Global Energy Investment.',
+    description: 'Metadata lorem ipsum sit amet. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.',
+    status: 'active',
+    defaultSubgroup: 'energy-balance',
+    subgroups: [
+      { id: 1, name: 'Energy Balance', slug: 'energy-balance' },
+      { id: 2, name: 'Energy Supply', slug: 'energy-supply' },
+      { id: 3, name: 'Energy Efficiency', slug: 'energy-efficiency' },
+      { id: 4, name: 'Energy Trade', slug: 'energy-trade' },
+      { id: 5, name: 'Energy Investment', slug: 'energy-investment' },
+      { id: 6, name: 'Electricity', slug: 'rlectricity' },
+      { id: 7, name: 'Energy Price', slug: 'energy-price' },
+      { id: 8, name: 'Energy Technology', slug: 'energy-technology' },
+      { id: 9, name: 'Energy Consumption', slug: 'energy-consumption' },
+      { id: 10, name: 'Internantional Comparison', slug: 'internantional-comparison' },
+      { id: 11, name: 'Emissions & Natural Disaters', slug: 'emissions&natural-disaters' },
+      { id: 12, name: 'Transporation sector', slug: 'transporation-sector' },
+      { id: 13, name: 'Industry sector', slug: 'industry-sector' },
+      { id: 14, name: 'Buildings', slug: 'buildings' },
+      { id: 15, name: 'Energy Resources Reserves', slug: 'energy-esources-reserves' },
+    ],
+  },
+  {
+    id: 2,
+    slug: 'socio-economic',
+    title: 'Socio economic',
+    subtitle: 'Agriculture.',
+    description: 'Metadata lorem ipsum sit amet. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.',
+    defaultSubgroup: 'transportation',
+    status: 'disabled',
+    subgroups: [
+      { id: 1, name: 'Transportation', slug: 'transportation' },
+      { id: 2, name: 'Energy Supply', slug: 'energy-supply' },
+      { id: 3, name: 'Energy Efficiency', slug: 'energy-efficiency' },
+      { id: 4, name: 'Energy Trade', slug: 'energy-trade' },
+      { id: 5, name: 'Energy Investment', slug: 'energy-investment' },
+      { id: 6, name: 'Electricity', slug: 'rlectricity' },
+      { id: 7, name: 'Energy Price', slug: 'energy-price' },
+      { id: 8, name: 'Energy Technology', slug: 'energy-technology' },
+      { id: 9, name: 'Energy Consumption', slug: 'energy-consumption' },
+      { id: 10, name: 'Internantional Comparison', slug: 'internantional-comparison' },
+      { id: 11, name: 'Emissions & Natural Disaters', slug: 'emissions&natural-disaters' },
+      { id: 12, name: 'Transporation sector', slug: 'transporation-sector' },
+      { id: 13, name: 'Industry sector', slug: 'industry-sector' },
+      { id: 14, name: 'Buildings', slug: 'buildings' },
+      { id: 15, name: 'Energy Resources Reserves', slug: 'energy-esources-reserves' },
+    ],
+  },
+  {
+    id: 3,
+    slug: 'coal-power-plant',
+    title: 'Coal power plants',
+    subtitle: 'Capacity power plants in China.',
+    description: 'Metadata lorem ipsum sit amet. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.',
+    defaultSubgroup: 'capacity',
+    status: 'disabled',
+    subgroups: [
+      { id: 1, name: 'Capacity', slug: 'capacity' },
+      { id: 2, name: 'Energy Supply', slug: 'energy-supply' },
+      { id: 3, name: 'Energy Efficiency', slug: 'energy-efficiency' },
+      { id: 4, name: 'Energy Trade', slug: 'energy-trade' },
+      { id: 5, name: 'Energy Investment', slug: 'energy-investment' },
+      { id: 6, name: 'Electricity', slug: 'rlectricity' },
+      { id: 7, name: 'Energy Price', slug: 'energy-price' },
+      { id: 8, name: 'Energy Technology', slug: 'energy-technology' },
+      { id: 9, name: 'Energy Consumption', slug: 'energy-consumption' },
+      { id: 10, name: 'Internantional Comparison', slug: 'internantional-comparison' },
+      { id: 11, name: 'Emissions & Natural Disaters', slug: 'emissions&natural-disaters' },
+      { id: 12, name: 'Transporation sector', slug: 'transporation-sector' },
+      { id: 13, name: 'Industry sector', slug: 'industry-sector' },
+      { id: 14, name: 'Buildings', slug: 'buildings' },
+      { id: 15, name: 'Energy Resources Reserves', slug: 'energy-esources-reserves' },
+    ],
+  },
+  {
+    id: 4,
+    slug: 'lorem ipsum',
+    title: 'lorem ipsum',
+    subtitle: 'lorem ipsum in China.',
+    description: 'Metadata lorem ipsum sit amet. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.',
+    defaultSubgroup: 'energy-balance',
+    status: 'disabled',
+    subgroups: [
+      { id: 1, name: 'Energy Balance', slug: 'energy-balance' },
+      { id: 2, name: 'Energy Supply', slug: 'energy-supply' },
+      { id: 3, name: 'Energy Efficiency', slug: 'energy-efficiency' },
+      { id: 4, name: 'Energy Trade', slug: 'energy-trade' },
+      { id: 5, name: 'Energy Investment', slug: 'energy-investment' },
+      { id: 6, name: 'Electricity', slug: 'rlectricity' },
+      { id: 7, name: 'Energy Price', slug: 'energy-price' },
+      { id: 8, name: 'Energy Technology', slug: 'energy-technology' },
+      { id: 9, name: 'Energy Consumption', slug: 'energy-consumption' },
+      { id: 10, name: 'Internantional Comparison', slug: 'internantional-comparison' },
+      { id: 11, name: 'Emissions & Natural Disaters', slug: 'emissions&natural-disaters' },
+      { id: 12, name: 'Transporation sector', slug: 'transporation-sector' },
+      { id: 13, name: 'Industry sector', slug: 'industry-sector' },
+      { id: 14, name: 'Buildings', slug: 'buildings' },
+      { id: 15, name: 'Energy Resources Reserves', slug: 'energy-esources-reserves' },
+    ],
+  },
+  {
+    id: 5,
+    slug: 'lorem ipsum2',
+    title: 'lorem ipsum',
+    subtitle: 'lorem ipsum in China.',
+    description: 'Metadata lorem ipsum sit amet. Donec ullamcorper nulla non metus auctor fringilla. Donec ullamcorper nulla non metus auctor fringilla. Vivamus sagittis lacus vel augue laoreet rutrum faucibus.',
+    defaultSubgroup: 'energy-balance',
+    status: 'disabled',
+    subgroups: [
+      { id: 1, name: 'Energy Balance', slug: 'energy-balance' },
+      { id: 2, name: 'Energy Supply', slug: 'energy-supply' },
+      { id: 3, name: 'Energy Efficiency', slug: 'energy-efficiency' },
+      { id: 4, name: 'Energy Trade', slug: 'energy-trade' },
+      { id: 5, name: 'Energy Investment', slug: 'energy-investment' },
+      { id: 6, name: 'Electricity', slug: 'rlectricity' },
+      { id: 7, name: 'Energy Price', slug: 'energy-price' },
+      { id: 8, name: 'Energy Technology', slug: 'energy-technology' },
+      { id: 9, name: 'Energy Consumption', slug: 'energy-consumption' },
+      { id: 10, name: 'Internantional Comparison', slug: 'internantional-comparison' },
+      { id: 11, name: 'Emissions & Natural Disaters', slug: 'emissions&natural-disaters' },
+      { id: 12, name: 'Transporation sector', slug: 'transporation-sector' },
+      { id: 13, name: 'Industry sector', slug: 'industry-sector' },
+      { id: 14, name: 'Buildings', slug: 'buildings' },
+      { id: 15, name: 'Energy Resources Reserves', slug: 'energy-esources-reserves' },
+    ],
+  }];
+
+export const SUBGROUPS = [
+  {
+    slug: 'energy-balance', id: 1, name: 'Energy Balance', group: '1',
+  },
+  {
+    slug: 'energy-supply', id: 2, name: 'Energy Supply', group: '1',
+  },
+  {
+    slug: 'energy-efficiency', id: 3, name: 'Energy Efficiency', group: '1',
+  },
+  {
+    slug: 'energy-trade', id: 4, name: 'Energy Trade', group: '1',
+  },
+  {
+    slug: 'energy-investment', id: 5, name: 'Energy Investment', group: '1',
+  },
+  {
+    slug: 'electricity', id: 6, name: 'Electricity', group: '1',
+  },
+  {
+    slug: 'energy-price', id: 7, name: 'Energy Price', group: '1',
+  },
+  {
+    slug: 'energy-technology', id: 8, name: 'Energy Technology', group: '1',
+  },
+  {
+    slug: 'energy-consumption', id: 9, name: 'Energy Consumption', group: '1',
+  },
+  {
+    slug: 'internantional-comparison', id: 10, name: 'Internantional Comparison', group: '1',
+  },
+  {
+    slug: 'emissions&natural-disaters', id: 11, name: 'Emissions & Natural Disaters', group: '1',
+  },
+  {
+    slug: 'transporation-sector', id: 12, name: 'Transporation sector', group: '1',
+  },
+  {
+    slug: 'industry-sector', id: 13, name: 'Industry sector', group: '1',
+  },
+  {
+    slug: 'buildings', id: 14, name: 'Buildings', group: '1',
+  },
+  {
+    slug: 'energy-resources-reserves', id: 15, name: 'Energy Resources Reserves', groupId: '1',
+  },
+  {
+    slug: 'Agriculture', id: 16, name: 'Agriculture', groupId: '2',
+  },
+  {
+    slug: 'building', id: 26, name: 'Building', groupId: '2',
+  },
+  {
+    slug: 'employment', id: 36, name: 'Employment', groupId: '2',
+  },
+  {
+    slug: 'exchange-rRate', id: 47, name: 'Exchange Rate', groupId: '2',
+  },
+  {
+    slug: 'GDP', id: 57, name: 'GDP', groupId: '2',
+  },
+  {
+    slug: 'imports&exports', id: 68, name: 'Imports and exports', groupId: '2',
+  },
+  {
+    slug: 'industry', id: 79, name: 'Industry', groupId: '2',
+  },
+  {
+    slug: 'whatever33', id: 89, name: 'Energy Technology', groupId: '2',
+  },
+  {
+    slug: 'whatever37', id: 99, name: 'Energy Consumption', group: '1',
+  },
+  {
+    slug: 'whatever99', id: 101, name: 'Internantional Comparison', group: '1',
+  },
+  {
+    slug: 'whatever100', id: 111, name: 'Emissions & Natural Disaters', group: '1',
+  },
+  {
+    slug: 'whatever222', id: 121, name: 'Transporation sector', group: '1',
+  },
+  {
+    slug: 'whatever33333', id: 131, name: 'Industry sector', group: '1',
+  },
+  {
+    slug: 'whatever451', id: 141, name: 'Buildings', group: '1',
+  },
+  {
+    slug: 'whatever432', id: 151, name: 'Energy Resources Reserves', group: '1',
+  },
+];
+
+export default {
+  GROUPS,
+  SUBGROUPS,
+};
