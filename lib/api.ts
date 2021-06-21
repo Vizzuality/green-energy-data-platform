90c45844114ac2e98cc7cfaf33af7dfab379bba1
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-06-21 16:50:09 +0200

    makes VERCEL_URL accessible to client

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: (process.env.NEXT_PUBLIC_API_URL || `${process.env.VERCEL_URL}/api`),
+  baseURL: (process.env.NEXT_PUBLIC_API_URL || `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`),
   headers: { 'Content-Type': 'application/json' },
 });
 
