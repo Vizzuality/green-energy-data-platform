8195ec4fa4a402819b147a6102c1e1f44a4a1d62
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-06-21 16:55:23 +0200

    fixes duplicated baseURL

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: (process.env.NEXT_PUBLIC_API_URL || `${process.env.NEXT_PUBLIC_VERCEL_URL}/api`),
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL ? '/api' : process.env.NEXT_PUBLIC_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
