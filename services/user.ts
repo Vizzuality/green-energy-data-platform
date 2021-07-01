7ebb383059ab5413caf668961ae6df3d488d1ad0
mluena <maluenarod@gmail.com>
2021-07-01 11:18:21 +0200

    comment removed

--- services/user.ts
+++ services/user.ts
@@ -2,9 +2,6 @@ import { API } from 'lib/api';
 
 export const fetchUsers = () => API.get('/users')
   .then(({ data }) => data);
-  // .catch((e) => {
-  //     console.log(e, 'error');
-  // });
 
 export const fetchUserMe = (
   userToken: string,
