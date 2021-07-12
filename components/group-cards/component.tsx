373d4007f18722c68212492e56ad866df39fe646
mluena <maluenarod@gmail.com>
2021-07-12 18:26:59 +0200

    prevent default subgroupf of being null

--- components/group-cards/component.tsx
+++ components/group-cards/component.tsx
@@ -25,14 +25,15 @@ const GroupCard: FC<GroupCardProps> = ({
         subtitle,
         description,
         default_subgroup,
+        subgroups,
       }, index) => (
         <div key={id} className={cx('flex justify-around', { 'flex-row-reverse': index % 2 !== 0 })}>
           <div className="m-16 max-w-md">
             <h3 className="text-3.5xl text-gray3">{name}</h3>
             <h4 className="text-2.5xl text-color1 pb-2 text-bold leading-loose">{subtitle}</h4>
             <p className="text-sm leading-7 my-9">{description}</p>
-            <Link href={`/${slug}/${default_subgroup}`} passHref>
-              <a href={`/${slug}/${default_subgroup}`} className="py-3 px-6 text-sm text-white rounded-full bg-gradient-color1">Discover</a>
+            <Link href={`/${slug}/${default_subgroup || subgroups[0]}`} passHref>
+              <a href={`/${slug}/${default_subgroup || subgroups[0]}`} className="py-3 px-6 text-sm text-white rounded-full bg-gradient-color1">Discover</a>
             </Link>
           </div>
           <img src={`/images/landing/${slug}.png`} alt={slug} className="rounded-4xl bg-shadow max-w-md m-16" />
