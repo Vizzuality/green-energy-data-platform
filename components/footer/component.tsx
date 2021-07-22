ba04b0ec97705dcee289e0db71a88bd049dcc855
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2021-07-22 18:32:58 +0200

    adds links to logo application

--- components/footer/component.tsx
+++ components/footer/component.tsx
@@ -17,7 +17,11 @@ const Footer: FC<FooterProps> = ({
 
   >
     <div className="flex w-full justify-around p-2 items-center">
-      <img alt="GEDP" src="/images/logo_GEDP.svg" className="w-28 h-auto" />
+      <Link href="/" passHref>
+        <a href="/">
+          <img alt="Green Energy Data Platform" src="/images/logo_GEDP.svg" className="w-28 h-auto" />
+        </a>
+      </Link>
       <div className="flex divide-x">
         <Link href="/privacy-policy" passHref>
           <a href="/privacy-policy" className="px-4 cursor-pointer">Privacy Policy</a>
