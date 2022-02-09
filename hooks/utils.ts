88202b3cc0544cf551ad03f98942aa2e1c80f0e0
mluena <maria.luena@vizzuality.com>
2022-02-09 16:24:05 +0100

    colors update

--- hooks/utils.ts
+++ hooks/utils.ts
@@ -2,16 +2,25 @@ import chroma from 'chroma-js';
 
 export const useColors = (
   colorsNumber: number,
-) => chroma.scale([
-  '#1B5183',
-  '#1E6D86',
-  '#2A8FAF',
-  '#C9E6E8',
-  '#929292',
-  '#766964',
-  '#F8981C',
-  '#760015',
-]).colors(colorsNumber || 0);
+) => {
+  if (colorsNumber <= 3) {
+    return ([
+      '#35B8E0',
+      '#FDD6AA',
+      '#A40505',
+    ]);
+  }
+  return chroma.scale([
+    '#1B5183',
+    '#1E6D86',
+    '#2A8FAF',
+    '#C9E6E8',
+    '#929292',
+    '#766964',
+    '#F8981C',
+    '#760015',
+  ]).colors(colorsNumber || 0);
+};
 
 export default {
   useColors,
