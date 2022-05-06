79c087e7140049f7182f8ca26e3f2efe82ce7861
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-05-06 05:48:08 +0200

    adding password error message

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -42,6 +42,8 @@ const SigninPage: FC<SigninProps> = ({
 
   const [isRecovery, setRecovery] = useState(false);
 
+  const [isPasswordFailed, setPasswordFailed] = useState(false);
+
   const handleChange = (type: string, e: FormEvent<HTMLInputElement>): void => {
     setCredentials({
       ...credentials,
@@ -57,8 +59,13 @@ const SigninPage: FC<SigninProps> = ({
     } = credentials;
 
     signIn('email-password', {
+      redirect: false,
       email,
       password,
+    }).then((res) => {
+      const { error }:any = res;
+      if (error) setPasswordFailed(true);
+      else router.push('/');
     });
   }, [credentials]);
 
@@ -80,10 +87,10 @@ const SigninPage: FC<SigninProps> = ({
   return (
     <LayoutPage className="bg-gradient-color1">
       <Head title="Welcome to Green Energy Data Platform" />
-      <main className="flex flex-col h-full w-full m-auto min-h-screen pb-20">
+      <main className="flex flex-col w-full h-full min-h-screen pb-20 m-auto">
         <Header />
-        <div className="max-w-6xl flex items-center h-full flex-1 justify-center p-12 m-auto">
-          <section className="flex flex-col flex-1 w-full h-full justify-between max-w-xs text-white mx-20">
+        <div className="flex items-center justify-center flex-1 h-full max-w-6xl p-12 m-auto">
+          <section className="flex flex-col justify-between flex-1 w-full h-full max-w-xs mx-20 text-white">
             <h1 className="text-5.5xl font-bold pt-7 tracking-tight">Sign in</h1>
             <img alt="Sign-up" src="/images/signup.svg" className="py-3" />
             <div className="h-0.2 bg-gradient-to-r from-white to-white-50" />
@@ -111,8 +118,8 @@ const SigninPage: FC<SigninProps> = ({
                 <div className="pb-6">
                   <label htmlFor="email" className="text-2.5xl font-bold">
                     Your email is:
-                    <div className="relative mb-10 sm:mb-4 font-normal">
-                      <Icon ariaLabel="mail-input" name="mail" size="lg" className="absolute -left-10 transform -translate-y-1/2 top-1/2 font-bold" />
+                    <div className="relative mb-10 font-normal sm:mb-4">
+                      <Icon ariaLabel="mail-input" name="mail" size="lg" className="absolute font-bold transform -translate-y-1/2 -left-10 top-1/2" />
                       <input
                         id="email"
                         name="email"
@@ -131,8 +138,8 @@ const SigninPage: FC<SigninProps> = ({
                   </label>
                   <label htmlFor="password" className="text-2.5xl font-bold">
                     Enter your password:
-                    <div className="relative mb-10 sm:mb-4 font-normal">
-                      <Icon ariaLabel="password-input" name="password" size="lg" className="absolute -left-10 transform -translate-y-1/2 top-1/2 font-bold" />
+                    <div className="relative mb-10 font-normal sm:mb-4">
+                      <Icon ariaLabel="password-input" name="password" size="lg" className="absolute font-bold transform -translate-y-1/2 -left-10 top-1/2" />
                       <input
                         id="password"
                         name="password"
@@ -145,13 +152,14 @@ const SigninPage: FC<SigninProps> = ({
                         required
                       />
                       <div className={cx('w-full h-0.7 rounded-sm bg-gray1 bg-opacity-20 mb-4',
-                        { 'bg-gradient-color1': credentials.password.length })}
+                        { 'bg-gradient-color1': credentials.password.length } && { 'bg-red-800': isPasswordFailed })}
                       />
                     </div>
+                    {isPasswordFailed && <div className="font-sans text-xs font-light text-red-800">THE PASSWORD DOESN&apos;T MATCH THE EMAIL</div>}
                   </label>
                   <button
                     type="button"
-                    className="w-full underline text-xs text-right"
+                    className="w-full text-xs text-right underline"
                     onClick={toggleRecovery}
                   >
                     I don&apos;t remember my password
@@ -169,9 +177,9 @@ const SigninPage: FC<SigninProps> = ({
             )}
 
             {isRecovery && (
-              <div className="min-h-1/2 inline-flex flex-col justify-between flex-1 w-full">
+              <div className="inline-flex flex-col justify-between flex-1 w-full min-h-1/2">
                 <div className="relative justify-items-start">
-                  <Icon ariaLabel="back-button" name="arrow" size="lg" className="absolute -left-10 top-3 transform rotate-180 font-bold" onClick={toggleRecovery} />
+                  <Icon ariaLabel="back-button" name="arrow" size="lg" className="absolute font-bold transform rotate-180 -left-10 top-3" onClick={toggleRecovery} />
                   <p className="text-2.5xl font-bold py-1">Forgot password</p>
                   <p>
                     Please enter your registered email and we will help
@@ -179,7 +187,7 @@ const SigninPage: FC<SigninProps> = ({
                   </p>
                   <div className="py-6">
                     <label htmlFor="email" className="text-2.5xl font-bold">
-                      <div className="relative mb-10 sm:mb-4 font-normal">
+                      <div className="relative mb-10 font-normal sm:mb-4">
                         <input
                           id="email"
                           name="email"
