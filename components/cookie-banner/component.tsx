7b5c3c3437eec4d19b2dd890af175ce13f81cfb2
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-05-11 17:28:33 +0200

    adding cookie link

--- components/cookie-banner/component.tsx
+++ components/cookie-banner/component.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import cx from 'classnames';
 
 export interface NavProps {
   className?: string;
@@ -10,11 +9,16 @@ export const cookieBanner: React.FC<NavProps> = ({
   className,
 }: NavProps) => (
   <>
-    <div className="absolute w-full h-16 text-center">
+    <div className="absolute w-full h-16 text-center bg-slate-800">
       <p>
         This site uses cookies to provide you with a great user experience.
-        By using this platform, you accept our use of cookies.
+        By using this platform, you accept our
+        <Link href={{ pathname: '/' }} passHref>
+          <a href="/" className="flex items-center justify-center text-center text-white bg-transparent border-2 border-white rounded-full hover:text-opacity-50 hover:border-opacity-50 active:bg-white active:text-black focus:outline-none">use of cookies.</a>
+        </Link>
       </p>
+
+      <button onClick={ }>X</button>
     </div>
   </>
 );
