f3e054b133f8f2ce9d5c331b2b031bd55af5d8aa
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-05-12 04:16:10 +0200

    fix duplicate branch & transltion for the download card

--- components/data-source/component.tsx
+++ components/data-source/component.tsx
@@ -4,6 +4,8 @@ import cx from 'classnames';
 import { useRouter } from 'next/router';
 
 import i18next from 'i18next';
+import { useSession } from 'next-auth/client';
+import Button from 'components/button';
 import Item from './item';
 
 interface DataProps {
@@ -25,41 +27,66 @@ const Card: FC<DataProps> = ({
   type = 'vertical',
   indicatorSlug,
   dataSource,
-  isAccesible = false,
+  isAccesible: isAccessible = false,
 }: DataProps) => {
   const router = useRouter();
+  const [session] = useSession();
   const { subgroup } = router.query;
+  const signInPage = () => router.push('/signin');
   const indSlug = indicatorSlug || subgroup?.[1];
 
   return (
     <div className={cx('flex divide-gray4 divide-opacity-90 text-center bg-gray5 text-gray1 rounded-2xl',
       {
         [className]: className,
-        'opacity-50': !isAccesible,
+        'opacity-50': !isAccessible,
         'divide-y flex-col': type === 'vertical',
         'divide-x flex-row': type === 'horizontal',
       })}
     >
-      <Item
-        icon="download"
-        name={i18next.t('download')}
-        links={downloadLinks}
-        className={cx('p-6',
-          { 'justify-center rounded-l-2xl': type === 'horizontal' },
-          { 'justify-start rounded-t-2xl': type === 'vertical' })}
-        indSlug={indSlug}
-        disabled={!isAccesible}
-      />
-      <Item
-        icon="data"
-        name={i18next.t('dataSource')}
-        source={dataSource}
-        className={cx('p-6',
-          { 'justify-center rounded-r-2xl': type === 'horizontal' },
-          { 'justify-start rounded-b-2xl': type === 'vertical' })}
-        indSlug={indSlug}
-        disabled={!isAccesible}
-      />
+      {!session
+        ? (
+          <div className="flex-col items-center p-6">
+            <p>{i18next.t('downloadLoginMode')}</p>
+            <Button
+              className="mx-auto mt-6"
+              type="submit"
+              aria-label="Sign in"
+              theme="secondary-background-dark"
+              size="xlg"
+              onClick={signInPage}
+            >
+              Log in
+            </Button>
+          </div>
+        )
+        : (
+
+          <>
+            <Item
+              icon="download"
+              name={i18next.t('download')}
+              links={downloadLinks}
+              className={cx('p-6',
+                { 'justify-center rounded-l-2xl': type === 'horizontal' },
+                { 'justify-start rounded-t-2xl': type === 'vertical' })}
+              indSlug={indSlug}
+              disabled={!isAccessible}
+            />
+            <Item
+              icon="data"
+              name={i18next.t('dataSource')}
+              source={dataSource}
+              className={cx('p-6',
+                { 'justify-center rounded-r-2xl': type === 'horizontal' },
+                { 'justify-start rounded-b-2xl': type === 'vertical' })}
+              indSlug={indSlug}
+              disabled={!isAccessible}
+            />
+
+          </>
+
+        )}
     </div>
   );
 };
