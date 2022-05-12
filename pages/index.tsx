d5e28b61ca3d9b3a14291e2da5070fdbbd4b8a34
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-05-12 20:14:44 +0200

    fixing pre-footer styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,19 +27,19 @@ const HomePage: FC = () => {
   return (
     <LayoutPage className="h-full min-h-screen">
       <Head title="Welcome to Green Energy Data Platform" />
-      <Hero className="flex flex-col text-center py-24 lg:px-32 md:px-24 sm:px-16 px-8">
+      <Hero className="flex flex-col px-8 py-24 text-center lg:px-32 md:px-24 sm:px-16">
         <h1 className="text-5.5xl pb-14">{i18next.t('landingTitle')}</h1>
         <h3 className="text-lg">Longer description about the site and benefits, lorem ipsum sit amet. Donec ullamcorper nulla non metus auctor fringilla.</h3>
       </Hero>
-      <div className="-mt-11 container m-auto lg:px-32 md:px-24 sm:px-16 px-8 mb-28">
+      <div className="container px-8 m-auto -mt-11 lg:px-32 md:px-24 sm:px-16 mb-28">
         <Search
           items={groups}
-          className="-mt-4 h-24 bg-white p-4 shadow-sm"
+          className="h-24 p-4 -mt-4 bg-white shadow-sm"
         >
           <Link href="/indicators" passHref>
             <a
               href="/indicators"
-              className="bg-gray1 text-white ml-3 border border-gray1 focus-within:flex items-center justify-center text-center rounded hover:bg-opacity-90 active:bg-white active:text-gray1  focus:outline-none py-5 px-11 text-sm"
+              className="items-center justify-center py-5 ml-3 text-sm text-center text-white border rounded bg-gray1 border-gray1 focus-within:flex hover:bg-opacity-90 active:bg-white active:text-gray1 focus:outline-none px-11"
             >
               {i18next.t('browse')}
             </a>
@@ -53,16 +53,16 @@ const HomePage: FC = () => {
           return (
             <InView key={group} triggerOnce>
               {({ ref, inView }) => (
-                <div ref={ref} key={group.id} className="container m-auto pb-28 lg:px-32 md:px-24 sm:px-16 px-8">
+                <div ref={ref} key={group.id} className="container px-8 m-auto pb-28 lg:px-32 md:px-24 sm:px-16">
                   {inView && <GroupCard textPosition={textPosition} group={group} className={cx('flex justify-between items-start', { 'flex-row-reverse': textPosition === 'left' })} />}
                 </div>
               )}
             </InView>
           );
         })}
-      <section className="flex pb-23">
-        <PreFooter />
-      </section>
+
+      <PreFooter />
+
     </LayoutPage>
   );
 };
